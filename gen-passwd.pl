3a03a8c7533ab82e9966f4b758bc347307f8bce1
Mohd Faizal <faizal@geekshelves.com>
2013-08-21 10:27:02 +0800

    Commit SMTP password hash generator script

---
+++ gen-passwd.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl -w
+########################################################
+# @author wackoen
+# @wackoen@geekshelves.com
+# @Description 
+#   * generate authentication for postfix smtp telnet
+#
+########################################################
+
+use MIME::Base64;
+use feature 'switch';
+sub interactive
+{
+    $noargv = scalar(@ARGV);
+    
+    if($noargv > 0)
+    {
+        print "Application : gen-passwd \nDescription : Postfix SMTP Password generator for telnet usage \nVersion : 1.0";
+        print "\nNon Intractive mode are not available in current version\n\t Do use ./gen-passwd.pl or perl gen-passwd.pl\n\n ";
+    }
+    else
+    {
+        # Allow user to key info input by input
+        print "Enter Domain : ";
+        chomp($domain = <>);
+
+        print "Enter Username : ";
+        chomp($username = <>);
+
+        print "Enter Password : ";
+        system('stty','-echo');
+        chop($password=<STDIN>);
+        system('stty','echo');
+    
+    
+        $coded = "\000".$username."\@".$domain."\000".$password;
+        $encoded = encode_base64($coded);
+        print "\nTelnet auth plain : ";
+        print $encoded;
+    }
+}
+
+interactive();
